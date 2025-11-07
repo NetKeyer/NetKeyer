fc3254dce2edc1623af5612c521cdb6cf87c093f
Andrew Rodland <andrew@cleverdomain.org>
2025-11-07 14:38:06 -0500

    WIP velopack

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using System;
+using Velopack;
 
 namespace NetKeyer;
 
@@ -9,8 +10,13 @@ sealed class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        // Velopack: Handle app installation/update events before starting the main app
+        VelopackApp.Build().Run();
+
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
