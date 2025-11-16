64591f9206a9a657f97674dfc834e37a849686f6
Andrew Rodland <andrew@cleverdomain.org>
2025-11-16 03:12:03 -0500

    handle "running status" in MIDI
    MIDI uses bytes 0x80..0xff as "status bytes" (i.e. commands), and bytes
    0x00..0x7f as value bytes (this is why most values are limited to
    0-127). There's an optimization called "running status", in which, if
    multiple events have the same status byte, the status byte is omitted,
    and the device just sends a stream of data bytes (which we recognize
    because they're < 0x80).
    
    The MIDI library we're using doesn't do any of that for us so we have to
    do it ourselves.

--- Midi/MidiPaddleInput.cs
+++ Midi/MidiPaddleInput.cs
@@ -11,11 +11,13 @@ namespace NetKeyer.Midi
         private const int RIGHT_PADDLE_NOTE = 21;
         private const byte NOTE_ON = 0x90;
         private const byte NOTE_OFF = 0x80;
+        private const bool DEBUG_MIDI = false; // Set to true to enable MIDI debug logging
 
         private IMidiAccess _access;
         private IMidiInput _inputPort;
         private bool _leftPaddleState = false;
         private bool _rightPaddleState = false;
+        private byte _lastStatusByte = 0; // Track last status byte for running status
 
         public event EventHandler<PaddleStateChangedEventArgs> PaddleStateChanged;
 
@@ -78,56 +80,163 @@ namespace NetKeyer.Midi
                 }
             }
 
-            // Reset paddle states
+            // Reset paddle states and running status
             _leftPaddleState = false;
             _rightPaddleState = false;
+            _lastStatusByte = 0;
         }
 
         private void OnMidiMessageReceived(object sender, MidiReceivedEventArgs e)
         {
-            var data = e.Data;
-
-            if (data.Length < 3)
-                return;
-
-            byte status = data[0];
-            byte messageType = (byte)(status & 0xF0);
-            byte note = data[1];
-            byte velocity = data[2];
-
-            if (messageType == NOTE_ON)
+            try
             {
-                HandleNoteEvent(note, true);
+                var data = e.Data;
+                var start = e.Start;
+                var length = e.Length;
+
+                if (length == 0)
+                    return;
+
+                if (DEBUG_MIDI)
+                {
+                    var relevantBytes = data.Skip(start).Take(length);
+                    Console.WriteLine($"[MIDI Raw] data.Length={data.Length} start={start} length={length} data=[{string.Join(" ", relevantBytes.Select(b => $"{b:X2}"))}]");
+                }
+
+                // Parse all MIDI messages in the buffer
+                int pos = start;
+                int end = start + length;
+
+                while (pos < end)
+                {
+                    byte statusByte;
+                    int dataStart;
+
+                    // Check if this is a status byte or running status
+                    if (data[pos] >= 0x80)
+                    {
+                        // New status byte
+                        statusByte = data[pos];
+                        _lastStatusByte = statusByte;
+                        dataStart = pos + 1;
+                        if (DEBUG_MIDI)
+                            Console.WriteLine($"[MIDI] Status byte 0x{statusByte:X2} at pos {pos}");
+                    }
+                    else
+                    {
+                        // Running status - reuse last status byte
+                        statusByte = _lastStatusByte;
+                        dataStart = pos;
+                        if (DEBUG_MIDI)
+                            Console.WriteLine($"[MIDI] Running status, reusing 0x{statusByte:X2}");
+                    }
+
+                    // Get the number of data bytes for this message type
+                    int dataSize = MidiEvent.FixedDataSize(statusByte);
+                    if (DEBUG_MIDI)
+                        Console.WriteLine($"[MIDI] Message requires {dataSize} data bytes");
+
+                    // Make sure we have enough data
+                    if (dataStart + dataSize > end)
+                    {
+                        Console.WriteLine($"[MIDI ERROR] Incomplete message: need {dataSize} bytes but only {end - dataStart} available");
+                        break;
+                    }
+
+                    // Parse the message based on type
+                    byte messageType = (byte)(statusByte & 0xF0);
+
+                    if (dataSize >= 1)
+                    {
+                        byte note = data[dataStart];
+                        byte velocity = dataSize >= 2 ? data[dataStart + 1] : (byte)0;
+
+                        if (DEBUG_MIDI)
+                            Console.WriteLine($"[MIDI Event] StatusByte=0x{statusByte:X2} Note={note} Velocity={velocity}");
+
+                        if (messageType == NOTE_ON)
+                        {
+                            HandleNoteEvent(note, true);
+                        }
+                        else if (messageType == NOTE_OFF)
+                        {
+                            HandleNoteEvent(note, false);
+                        }
+                    }
+
+                    // Move to next message
+                    pos = dataStart + dataSize;
+                }
+
+                if (DEBUG_MIDI)
+                    Console.WriteLine($"[MIDI] OnMidiMessageReceived complete");
             }
-            else if (messageType == NOTE_OFF)
+            catch (Exception ex)
             {
-                HandleNoteEvent(note, false);
+                Console.WriteLine($"[MIDI ERROR] Exception in OnMidiMessageReceived: {ex.Message}");
+                Console.WriteLine($"[MIDI ERROR] Stack trace: {ex.StackTrace}");
             }
         }
 
         private void HandleNoteEvent(int noteNumber, bool isOn)
         {
-            bool stateChanged = false;
+            if (DEBUG_MIDI)
+                Console.WriteLine($"[MIDI] Note {noteNumber} {(isOn ? "ON" : "OFF")}");
 
-            if (noteNumber == LEFT_PADDLE_NOTE && _leftPaddleState != isOn)
+            if (noteNumber == LEFT_PADDLE_NOTE)
             {
-                _leftPaddleState = isOn;
-                stateChanged = true;
-            }
-            else if (noteNumber == RIGHT_PADDLE_NOTE && _rightPaddleState != isOn)
-            {
-                _rightPaddleState = isOn;
-                stateChanged = true;
-            }
-
-            if (stateChanged)
-            {
-                PaddleStateChanged?.Invoke(this, new PaddleStateChangedEventArgs
+                // Handle note OFF when we didn't see note ON - this means the paddle
+                // was pressed while the other paddle was already held (MIDI device limitation)
+                if (!isOn && !_leftPaddleState)
                 {
-                    LeftPaddle = _leftPaddleState,
-                    RightPaddle = _rightPaddleState
-                });
+                    if (DEBUG_MIDI)
+                        Console.WriteLine($"[MIDI] Left paddle OFF without ON - treating as brief press/release");
+                    // Fire event for both ON and OFF to simulate the missed press
+                    _leftPaddleState = true;
+                    PaddleStateChanged?.Invoke(this, new PaddleStateChangedEventArgs
+                    {
+                        LeftPaddle = _leftPaddleState,
+                        RightPaddle = _rightPaddleState
+                    });
+                }
+
+                _leftPaddleState = isOn;
+                if (DEBUG_MIDI)
+                    Console.WriteLine($"[MIDI] Left paddle -> {isOn}");
             }
+            else if (noteNumber == RIGHT_PADDLE_NOTE)
+            {
+                // Handle note OFF when we didn't see note ON
+                if (!isOn && !_rightPaddleState)
+                {
+                    if (DEBUG_MIDI)
+                        Console.WriteLine($"[MIDI] Right paddle OFF without ON - treating as brief press/release");
+                    _rightPaddleState = true;
+                    PaddleStateChanged?.Invoke(this, new PaddleStateChangedEventArgs
+                    {
+                        LeftPaddle = _leftPaddleState,
+                        RightPaddle = _rightPaddleState
+                    });
+                }
+
+                _rightPaddleState = isOn;
+                if (DEBUG_MIDI)
+                    Console.WriteLine($"[MIDI] Right paddle -> {isOn}");
+            }
+            else
+            {
+                if (DEBUG_MIDI)
+                    Console.WriteLine($"[MIDI] Ignoring unknown note {noteNumber}");
+                return; // Don't fire event for unknown notes
+            }
+
+            if (DEBUG_MIDI)
+                Console.WriteLine($"[MIDI] Firing event: L={_leftPaddleState} R={_rightPaddleState}");
+            PaddleStateChanged?.Invoke(this, new PaddleStateChangedEventArgs
+            {
+                LeftPaddle = _leftPaddleState,
+                RightPaddle = _rightPaddleState
+            });
         }
 
         public void Dispose()
