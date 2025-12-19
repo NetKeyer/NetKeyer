bfdaa28c087a014cdebfab032941161ac9234e15
Andrew Rodland <andrew@cleverdomain.org>
2025-12-19 18:15:35 -0500

    get a more native experience on macOS
    Make the app name show up as "NetKeyer" instead of "Avalonia Application", and
    move the menu bar to the mac native menu bar instead of having it appear in
    the window.

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
+using System.Runtime.InteropServices;
 using Avalonia.Controls;
+using NetKeyer.ViewModels;
 
 namespace NetKeyer.Views;
 
@@ -7,5 +9,65 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        
+        // Set up native macOS menu bar
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            SetupMacOsNativeMenu();
+        }
+    }
+    
+    private void SetupMacOsNativeMenu()
+    {
+        // Create the native menu for macOS
+        var nativeMenu = new NativeMenu();
+        
+        // File menu
+        var fileMenu = new NativeMenuItem("File");
+        var fileSubMenu = new NativeMenu();
+        
+        var exitItem = new NativeMenuItem("Exit");
+        exitItem.Click += (s, e) =>
+        {
+            if (DataContext is MainWindowViewModel vm)
+            {
+                vm.ExitCommand?.Execute(null);
+            }
+        };
+        fileSubMenu.Add(exitItem);
+        fileMenu.Menu = fileSubMenu;
+
+        // Help menu
+        var helpMenu = new NativeMenuItem("Help");
+        var helpSubMenu = new NativeMenu();
+        
+        var documentationItem = new NativeMenuItem("Documentation");
+        documentationItem.Click += (s, e) =>
+        {
+            if (DataContext is MainWindowViewModel vm)
+            {
+                vm.OpenDocumentationCommand?.Execute(null);
+            }
+        };
+        
+        var aboutItem = new NativeMenuItem("About NetKeyer...");
+        aboutItem.Click += (s, e) =>
+        {
+            if (DataContext is MainWindowViewModel vm)
+            {
+                vm.ShowAboutCommand?.Execute(null);
+            }
+        };
+        
+        helpSubMenu.Add(documentationItem);
+        helpSubMenu.Add(aboutItem);
+        helpMenu.Menu = helpSubMenu;
+        
+        // Add menus to the native menu bar
+        nativeMenu.Add(fileMenu);
+        nativeMenu.Add(helpMenu);
+        
+        // Set the native menu for this window
+        NativeMenu.SetMenu(this, nativeMenu);
     }
 }
